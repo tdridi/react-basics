1b40301129ecc793c03d89fb1af4bc3df3e5c23d
Talel <taleldridi20gmail.com>
2018-10-23 15:35:29 -0400

    Componenet Life cycle

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -4,18 +4,17 @@
 
 import React from "react";
 
-export class Header extends React.Component {
-    render() {
-        return (
-            <nav className="navbar navbar-default">
-                <div className="container">
-                    <div className="navbar-header">
-                        <ul className="nav navbar-nav">
-                            <li><a href="">Home  fddf</a></li>
-                        </ul>
-                    </div>
+export const Header = (props) => {
+    return (
+        <nav className="navbar navbar-default">
+            <div className="container">
+                <div className="navbar-header">
+                    <ul className="nav navbar-nav">
+                        <li><a href="">{props.homeLink}</a></li>
+                    </ul>
                 </div>
-            </nav>
-        );
-    }
-}
+            </div>
+        </nav>
+    );
+
+};
